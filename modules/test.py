8b2ad4230b2039737254b7dfe4ae477b046dde72
leddd <38965971+leddd@users.noreply.github.com>
2024-07-20 22:57:57 -0400

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -1,6 +1,6 @@
 from pyo import *
 
-s = Server().boot()
+s = Server(sr=48000, nchnls=2, ichnls=1, audio='portaudio').boot()
 s.start()
 f = Fader(fadeout=0.2, dur = 0.1 , mul=.2)
 sine = Sine(freq=[500], mul=f).out()
