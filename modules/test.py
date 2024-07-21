36ecb4e459037e7183a9b004b4ef3516c4da746d
leddd <38965971+leddd@users.noreply.github.com>
2024-07-20 23:20:03 -0400

    Update test.py

--- modules/test.py
+++ modules/test.py
@@ -1,7 +1,7 @@
 from pyo import *
 
 # Initialize Pyo server with JACK audio
-s = Server(audio='portaudio').boot()
+s = Server(audio='jack').boot()
 s.start()
 
 # Create an audio effect with a sine wave
