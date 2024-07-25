534e9462b91d37af58d97f6574cf51b4bb1249f1
leddd <38965971+leddd@users.noreply.github.com>
2024-07-25 17:26:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ s.start()
 bpm = 120
 beats_per_bar = 4
 total_bars = 2
-latency = 0  # Latency in seconds
+latency = 0.05  # Latency in seconds
 
 class Metronome:
     def __init__(self, bpm, beats_per_bar, total_bars):
