f2ecb7299a95460d0381f6f8b935e091cd88894b
leddd <38965971+leddd@users.noreply.github.com>
2024-07-27 11:11:50 -0400

    Update looper.py

--- looper.py
+++ looper.py
@@ -133,7 +133,7 @@ class Track:
             self.playback = Looper(table=self.table, dur=self.metronome.duration, mul=20, xfade=0)
             self.highpass = ButHP(self.playback, freq=self.hp_freq)  # Apply highpass filter
             self.lowpass = ButLP(self.highpass, freq=self.lp_freq)
-            self.ex = Expand(self.lowpass, downthresh=-90, upthresh=-90, ratio=1.5, mul=0.1)
+            self.ex = Expand(self.lowpass, downthresh=-90, upthresh=-90, ratio=1.5, mul=0.2).out()
             #self.harm = Harmonizer(self.ex, transpo=0, winsize=0.05).out()
             self.master_trig = CallAfter(self.start_recording, latency)
 
@@ -156,7 +156,7 @@ class Track:
             self.playback = Looper(table=self.table, dur=self.metronome.duration, mul=20, xfade=0)
             self.highpass = ButHP(self.playback, freq=self.hp_freq)  # Apply highpass filter
             self.lowpass = ButLP(self.highpass, freq=self.lp_freq)
-            self.ex = Expand(self.lowpass, downthresh=-90, upthresh=-90, ratio=1.5, mul=0.1)
+            self.ex = Expand(self.lowpass, downthresh=-90, upthresh=-90, ratio=1.5, mul=0.2).out()
             #self.harm = Harmonizer(self.ex, transpo=0, winsize=0.05).out()
             self.track_trig = CallAfter(self.start_recording, latency)
             self.initialized = True
