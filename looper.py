49b6b9f5b53e1c4420206e22ad09496bea40168a
leddd <38965971+leddd@users.noreply.github.com>
2024-07-27 11:20:41 -0400

    Update looper.py

--- looper.py
+++ looper.py
@@ -20,7 +20,7 @@ config_option_values = {
     "TIME SIGNATURE": "4/4",
     "TOTAL BARS": 2
 }
-latency = 0.1  # Latency in seconds
+latency = 0.2  # Latency in seconds
 
 # Dictionary to store image paths for each time signature
 beat_images = {
@@ -133,8 +133,8 @@ class Track:
             self.playback = Looper(table=self.table, dur=self.metronome.duration, mul=20, xfade=0)
             self.highpass = ButHP(self.playback, freq=self.hp_freq)  # Apply highpass filter
             self.lowpass = ButLP(self.highpass, freq=self.lp_freq)
-            self.ex = Expand(self.lowpass, downthresh=-90, upthresh=-90, ratio=1.5, mul=0.2).out()
-            #self.harm = Harmonizer(self.ex, transpo=0, winsize=0.05).out()
+            self.ex = Expand(self.lowpass, downthresh=-90, upthresh=-90, ratio=1.5, mul=0.15)
+            self.harm = Harmonizer(self.ex, transpo=0, winsize=0.05).out()
             self.master_trig = CallAfter(self.start_recording, latency)
 
         if self.metronome.countdown_counter.get() == self.metronome.beats_per_bar * (1 + self.metronome.total_bars) + 1:
@@ -156,8 +156,8 @@ class Track:
             self.playback = Looper(table=self.table, dur=self.metronome.duration, mul=20, xfade=0)
             self.highpass = ButHP(self.playback, freq=self.hp_freq)  # Apply highpass filter
             self.lowpass = ButLP(self.highpass, freq=self.lp_freq)
-            self.ex = Expand(self.lowpass, downthresh=-90, upthresh=-90, ratio=1.5, mul=0.2).out()
-            #self.harm = Harmonizer(self.ex, transpo=0, winsize=0.05).out()
+            self.ex = Expand(self.lowpass, downthresh=-90, upthresh=-90, ratio=1.5, mul=0.15)
+            self.harm = Harmonizer(self.ex, transpo=0, winsize=0.05).out()
             self.track_trig = CallAfter(self.start_recording, latency)
             self.initialized = True
         
