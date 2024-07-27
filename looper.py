c2b6268daab9ab1d8f6ef47a024bfe25fb7ed5e0
leddd <38965971+leddd@users.noreply.github.com>
2024-07-27 11:47:51 -0400

    Update looper.py

--- looper.py
+++ looper.py
@@ -132,8 +132,8 @@ class Track:
             self.recorder = TableRec(self.input, table=self.table, fadetime=0.005)
             self.playback = Looper(table=self.table, dur=self.metronome.duration, mul=20, xfade=0)
             self.highpass = ButHP(self.playback, freq=self.hp_freq)  # Apply highpass filter
-            self.ex = ButLP(self.highpass, freq=self.lp_freq)
-            # self.ex = Expand(self.lowpass, downthresh=-90, upthresh=-90, ratio=2, mul=0.1)
+            self.lowpass = ButLP(self.highpass, freq=self.lp_freq)
+            self.ex = Expand(self.lowpass, downthresh=-90, upthresh=-90, ratio=2, mul=0.15)
             # self.harm = Harmonizer(self.ex, transpo=0, winsize=0.05).out()
             self.b = Compress(self.ex, thresh=-30, ratio=2, risetime=.01, falltime=.2, knee=0.5).out()
             self.master_trig = CallAfter(self.start_recording, latency)
@@ -156,8 +156,8 @@ class Track:
             self.recorder = TableRec(self.input, table=self.table, fadetime=0.01).out()
             self.playback = Looper(table=self.table, dur=self.metronome.duration, mul=20, xfade=0)
             self.highpass = ButHP(self.playback, freq=self.hp_freq)  # Apply highpass filter
-            self.ex = ButLP(self.highpass, freq=self.lp_freq)
-            # self.ex = Expand(self.lowpass, downthresh=-90, upthresh=-90, ratio=2, mul=0.1)
+            self.lowpass = ButLP(self.highpass, freq=self.lp_freq)
+            self.ex = Expand(self.lowpass, downthresh=-90, upthresh=-90, ratio=2, mul=0.15)
             # self.harm = Harmonizer(self.ex, transpo=0, winsize=0.05).out()
             self.b = Compress(self.ex, thresh=-30, ratio=2, risetime=.01, falltime=.2, knee=0.5).out()
             self.track_trig = CallAfter(self.start_recording, latency)
